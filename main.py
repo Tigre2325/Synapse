f07682307264e4a1aceafb02dc350b1395604ba9
Tigre2325 <tigre2325.ab@gmail.com>
2020-06-20 17:11:35 +0200

    Update main.py
    I add a lot of features:
    - theme variable (array) to change quickly between dark and light theme (with the images folder);
    - language variable (array) to change the laguage (will be done later);
    - centerWidth and centerHeight variables (int) which stores the center of the window;
     - ruleGameButton (Button) to access the rule on the game page;
     - currentPlayer (Text) which display the current player's name;
    - instructions (array) which stores all the instructions on the game page;
    - piecesRemaining (Text) : 2 objects to show the number of pieces currently remaining.
    
    I also update the colors of the buttons.

--- main.py
+++ main.py
@@ -28,72 +28,98 @@ rules = Page(app)
 ###############################################################################
 # Graphical elements
 #######################################
+# Global application settings
+centerWidth = int(app.winfo_width() * 50 / 100)
+centerHeight = int(app.winfo_height() * 50 / 100)
+
+# TODO: creer un objet Theme qui permet plus facilement de changer de theme
+theme = [
+    "light_theme",
+    "dark_theme"
+]
+currentTheme = theme[0]
+
+# TODO: fichier contenant tous les textes dans un ordre precis pour ajouter des langues
+language = [
+    "English",
+    "Français"
+]
+
+#######################################
 # Homepage
 homeBackground = Image(
-    home, "./images/light_theme/background.png",
-    int(app.winfo_width() * 50 / 100),
-    int(app.winfo_height() * 50 / 100)
+    home,
+    "./images/{}/background.png".format(currentTheme),
+    centerWidth,
+    centerHeight
 )
 
 synapse = Text(
     home,
     "SYNAPSE",
-    int(app.winfo_width() * 50 / 100),
+    centerWidth,
     int(app.winfo_height() * 20 / 100),
     -150,
     "#502010"
 )
 
+authors = Text(
+    home,
+    "Made by Tigre2325,\n\n with the participation of Fukuro and CaptainRommel,\n\n and based on LARBROJEUX's Synapse game",
+    centerWidth,
+    int(app.winfo_height() * 93 / 100),
+    12
+)
 
 # Buttons on the homepage
 playButton = Button(
     home,
-    "./images/light_theme/button_background_3-1.png",
+    "./images/{}/button_background_165x55.png".format(currentTheme),
     "Play",
-    int(app.winfo_width() * 50 / 100),
-    int(app.winfo_height() * 60 / 100),
+    centerWidth,
+    int(app.winfo_height() * 50 / 100),
     32,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
 settingsButton = Button(
     home,
-    "./images/light_theme/button_background_3-1.png",
+    "./images/{}/button_background_165x55.png".format(currentTheme),
     "Settings",
-    int(app.winfo_width() * 50 / 100),
-    int(app.winfo_height() * 80 / 100),
+    centerWidth,
+    int(app.winfo_height() * 70 / 100),
     32,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
 quitAppButton = Button(
     home,
-    "./images/light_theme/button_background_3-1.png",
+    "./images/{}/button_background_165x55.png".format(currentTheme),
     "Quit",
     int(app.winfo_width() * 7 / 100),
     int(app.winfo_height() * 95 / 100),
     16,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
 ruleButton = Button(
     home,
-    "./images/light_theme/button_background_3-1.png",
+    "./images/{}/button_background_165x55.png".format(currentTheme),
     "Rules",
     int(app.winfo_width() * 93 / 100),
     int(app.winfo_height() * 95 / 100),
     16,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
 # Show all the objects on the home page canvas
 homeBackground.show()
-
 synapse.show()
+authors.show()
 
 playButton.scale(2)
 playButton.show()
@@ -116,129 +142,200 @@ ruleButton.show()
 # Game page
 gameBackground = Image(
     game,
-    "./images/light_theme/background.png",
-    int(app.winfo_width() * 50 / 100),
-    int(app.winfo_height() * 50 / 100)
+    "./images/{}/background.png".format(currentTheme),
+    centerWidth,
+    centerHeight
 )
 
 board = Image(
     game,
-    "./images/light_theme/board.png",
-    int(app.winfo_width() * 50 / 100),
-    int(app.winfo_height() * 50 / 100)
+    "./images/{}/board.png".format(currentTheme),
+    centerWidth,
+    centerHeight
 )
 
 # Left side
 quitGameButton = Button(
     game,
-    "./images/light_theme/button_background_3-1.png",
+    "./images/{}/button_background_165x55.png".format(currentTheme),
     "Quit",
-    int(app.winfo_width() * 7 / 100),
+    int(app.winfo_width() * 6 / 100),
     int(app.winfo_height() * 95 / 100),
     14,
-    "#ceaf91",
+    "#f0d0a0",
+    "#804030"
+)
+
+ruleGameButton = Button(
+    game,
+    "./images/{}/button_background_165x55.png".format(currentTheme),
+    "Rules",
+    int(app.winfo_width() * 16 / 100),
+    int(app.winfo_height() * 95 / 100),
+    14,
+    "#f0d0a0",
     "#804030"
 )
 
 # Right side
+currentPlayer = Text(
+    game,
+    # TODO: creer un objet joueur avec une méthode de classe qui retourne le joueur actuel
+    "{}'s turn".format("Anthony"),
+    int(app.winfo_width() * 89 / 100),
+    int(app.winfo_height() * 6 / 100),
+    24,
+)
+currentPlayer.set(width=280)
+
+instructions = [
+    # Cliquez sur une des cases du plateau afin de placer vos pièces
+    # Sélectionnez le nombre de pièce à placer
+    # Sélectionnez l'orientation des pièces à placer
+    # Validez le tour
+    # Choisissez un nombre de pièces !
+    # Choisissez moins de pièces !
+    # Choisissez une orientation pour vos pièces !
+    # Votre choix n'est pas valide, les pièces se chevauchent !
+    # Votre choix n'est pas valide, les pièces seraient en dehors du plateau !
+    # Cliquez sur le bouton de validation pour continuer à jouer ou sur quitter pour arrêter
+    "Click on one of the board cases to position your pieces",
+    "Select the number of pieces you want to play",
+    "Select the orientation of your pieces",
+    # TODO: terminer l'écriture des instructions
+]
+
+instruction = Text(
+    game,
+    instructions[0],
+    int(app.winfo_width() * 89 / 100),
+    int(app.winfo_height() * 16 / 100)
+)
+instruction.set(width=280)
+
 onePieceButton = Button(
     game,
-    "./images/light_theme/number_piece.png",
+    "./images/{}/background_55x55.png".format(currentTheme),
     "1",
     int(app.winfo_width() * 82 / 100),
     int(app.winfo_height() * 30 / 100),
     22,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
 twoPieceButton = Button(
     game,
-    "./images/light_theme/number_piece.png",
+    "./images/{}/background_55x55.png".format(currentTheme),
     "2",
     int(app.winfo_width() * 89 / 100),
     int(app.winfo_height() * 30 / 100),
     22,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
 threePieceButton = Button(
     game,
-    "./images/light_theme/number_piece.png",
+    "./images/{}/background_55x55.png".format(currentTheme),
     "3",
     int(app.winfo_width() * 96 / 100),
     int(app.winfo_height() * 30 / 100),
     22,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
 arrowUpButton = Button(
     game,
-    "./images/light_theme/arrow_up.png",
+    "./images/{}/arrow_up.png".format(currentTheme),
     "",
     int(app.winfo_width() * 89 / 100),
     int(app.winfo_height() * 43 / 100),
     1,
-    "#ceaf91",
-    "#804030"
+    "#f0d0a0",
+    "#f0d0a0"
 )
 
 arrowDownButton = Button(
     game,
-    "./images/light_theme/arrow_down.png",
+    "./images/{}/arrow_down.png".format(currentTheme),
     "",
     int(app.winfo_width() * 89 / 100),
     int(app.winfo_height() * 61 / 100),
     1,
-    "#ceaf91",
-    "#804030"
+    "#f0d0a0",
+    "#f0d0a0"
 )
 
 arrowRightButton = Button(
     game,
-    "./images/light_theme/arrow_right.png",
+    "./images/{}/arrow_right.png".format(currentTheme),
     "",
     int(app.winfo_width() * 96 / 100),
     int(app.winfo_height() * 52 / 100),
     1,
-    "#ceaf91",
-    "#804030"
+    "#f0d0a0",
+    "#f0d0a0"
 )
 
 arrowLeftButton = Button(
     game,
-    "./images/light_theme/arrow_left.png",
+    "./images/{}/arrow_left.png".format(currentTheme),
     "",
     int(app.winfo_width() * 82 / 100),
     int(app.winfo_height() * 52 / 100),
     1,
-    "#ceaf91",
-    "#804030"
+    "#f0d0a0",
+    "#f0d0a0"
 )
 
 confirmButton = Button(
     game,
-    "./images/light_theme/button_background_3-1.png",
+    "./images/{}/button_background_165x55.png".format(currentTheme),
     "Confirm",
     int(app.winfo_width() * 89 / 100),
     int(app.winfo_height() * 73 / 100),
     20,
-    "#ceaf91",
+    "#f0d0a0",
     "#804030"
 )
 
-# Show all the objects on the home page canvas
-gameBackground.show()
+piecesRemainingText = Text(
+    game,
+    "Number of pieces remaining:",
+    int(app.winfo_width() * 89 / 100),
+    int(app.winfo_height() * 83 / 100),
+    20
+)
+piecesRemainingText.set(width=280)
 
+# TODO: nombre de pièce à créer avec le lancement de la partie
+piecesRemaining = 0
+
+piecesRemainingNumber = Text(
+    game,
+    "{}".format(piecesRemaining),
+    int(app.winfo_width() * 89 / 100),
+    int(app.winfo_height() * 93 / 100),
+    50
+)
+
+# Show all the objects on the home page canvas
+quitGameButton.show()
+quitGameButton.scale(0.6, 0.7)
+
+ruleGameButton.show()
+ruleGameButton.scale(0.6, 0.7)
+
+
+gameBackground.show()
 board.show()
 
-quitGameButton.scale(0.8, 0.7)
-quitGameButton.show()
 
-confirmButton.scale(1.2, 0.8)
-confirmButton.show()
+currentPlayer.show()
+
+instruction.show()
 
 onePieceButton.show()
 twoPieceButton.show()
@@ -248,6 +345,12 @@ arrowUpButton.show()
 arrowDownButton.show()
 arrowRightButton.show()
 arrowLeftButton.show()
+
+confirmButton.scale(1.2, 0.8)
+confirmButton.show()
+
+piecesRemainingText.show()
+piecesRemainingNumber.show()
 
 #######################################
 # Settings page
@@ -259,11 +362,11 @@ arrowLeftButton.show()
 
 #######################################
 # Open the start page
-# home.show(app)
-# register.show(app)
-game.show(app)
-# settings.show(app)
-# rules.show(app)
+# home.show()
+# register.show()
+game.show()
+# settings.show()
+# rules.show()
 ###############################################################################
 
 ###############################################################################
