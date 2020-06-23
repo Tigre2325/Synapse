af5b94dd5ab82771b52fd430c7aa095489bac261
Tigre2325 <tigre2325.ab@gmail.com>
2020-06-23 18:20:11 +0200

    Update page.py
    The page background is now directly set when the page is instanced.

--- classes/page.py
+++ classes/page.py
@@ -1,11 +1,14 @@
 import tkinter as tk
+from PIL import Image as ImageModule
+from PIL import ImageTk
 
 
 class Page(tk.Canvas):
     pageList = []
 
-    def __init__(self, parent):
+    def __init__(self, parent, background):
         self.parent = parent
+
         self.frame = tk.Frame(
             self.parent
         )
@@ -16,6 +19,19 @@ class Page(tk.Canvas):
             height=self.parent.height,
             borderwidth=0
         )
+
+        if "#" in background:
+            self.configure(background=background)
+        else:
+            self.image = ImageModule.open(background)
+            self.photoImage = ImageTk.PhotoImage(self.image)
+
+            self.create_image(
+                int(self.parent.winfo_width() * 50 / 100),
+                int(self.parent.winfo_height() * 50 / 100),
+                image=self.photoImage,
+            )
+
         self.frame.pack(fill=tk.BOTH, expand=True)  # Make the window resizable
         self.pack(expand=True)
         self.hide()
