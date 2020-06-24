236f9329740fd190c5274655d88218591e1ee8d7
Tigre2325 <tigre2325.ab@gmail.com>
2020-06-24 16:05:19 +0200

    Create settings.py
    This is the class where all the settings will be.
    It will be improved because for now you can't save your settings for later.

---
+++ classes/settings.py
@@ -0,0 +1,102 @@
+# libraires
+import tkinter as tk
+from PIL import Image as ImageModule
+from PIL import ImageTk
+from numpy import interp
+
+###############################################################################
+
+
+class Settings(tk.OptionMenu):
+    settingsList = []
+
+    def __init__(self, parent, imagePath, text, optionList, textColor, backgroundColor):
+        self.parent = parent
+        self.image = ImageModule.open(imagePath)
+        self.photoImage = ImageTk.PhotoImage(self.image)
+        self.text = text
+        self.optionList = optionList
+        self.textColor = textColor
+        self.backgroundColor = backgroundColor
+
+        # Canvas which will contain the setting
+        self.canvasSetting = tk.Canvas(
+            self.parent,
+            width=int(self.parent.winfo_reqwidth() * 60 / 100),
+            height=int(self.parent.winfo_reqheight() * 10 / 100),
+            borderwidth=10,
+            relief=tk.RIDGE,
+            background=backgroundColor,
+            highlightthickness=0
+        )
+
+        # Scaling the setting background image
+        self.xFactor = self.canvasSetting.winfo_reqwidth() / self.photoImage.width()
+        self.yFactor = self.canvasSetting.winfo_reqheight() / self.photoImage.height()
+        self.image = self.image.resize(
+            (int(self.image.width * self.xFactor),
+             int(self.image.height * self.yFactor)),
+            ImageModule.ANTIALIAS
+        )
+        self.photoImage = ImageTk.PhotoImage(self.image)
+
+        # Image on the canvas
+        self.canvasImage = self.canvasSetting.create_image(
+            int(self.canvasSetting.winfo_reqwidth() * 50 / 100),
+            int(self.canvasSetting.winfo_reqheight() * 50 / 100),
+            image=self.photoImage
+        )
+        # Text
+        self.canvasText = self.canvasSetting.create_text(
+            int(self.canvasSetting.winfo_reqwidth() * 25 / 100),
+            int(self.canvasSetting.winfo_reqheight() * 50 / 100),
+            text=self.text,
+            font=("Minecraft", 22),
+            fill=self.textColor,
+            justify=tk.CENTER
+        )
+
+        # Widget
+        def configureSetting(event):
+            print(self.variable.get())
+
+        self.variable = tk.StringVar()
+        self.variable.set(self.optionList[0])
+
+        tk.OptionMenu.__init__(
+            self,
+            self.parent,
+            self.variable,
+            *self.optionList,
+            command=configureSetting
+        )
+        self.configure(
+            width=12,
+            height=2,
+            background=textColor,
+            foreground=backgroundColor,
+            activebackground=backgroundColor,
+            activeforeground=textColor,
+            highlightthickness=0,
+            font=("Minecraft", 16),
+        )
+        self.canvasWidget = self.canvasSetting.create_window(
+            int(self.canvasSetting.winfo_reqwidth() * 75 / 100),
+            int(self.canvasSetting.winfo_reqheight() * 50 / 100),
+            window=self,
+        )
+
+        Settings.settingsList.append(self)
+
+    def show(self):
+        yPlace = interp(
+            Settings.settingsList.index(self),
+            [0, len(Settings.settingsList)],
+            [10, 100]
+        )
+        self.parent.create_window(
+            int(self.parent.winfo_reqwidth() * 60 / 100),
+            int(self.parent.winfo_reqheight() *
+                yPlace / 100),
+            window=self.canvasSetting
+        )
